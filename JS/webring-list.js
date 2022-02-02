0c1d1cc7bd4c172eac6b538ccf556c9dfadeab4e
Gray <https://discord.com/invite/JM34yvMaFP>
2022-02-02 14:49:55 -0500

    added WebRing section, fixed welcome.html expansion size on super-wide monitors.

--- JS/webring-list.js
+++ JS/webring-list.js
@@ -22,7 +22,7 @@ class Webrings extends HTMLElement {
         <th><a href="https://geekring.net/" target="_blank"> GeekRing </a></th>
         <th><a href="http://geekring.net/site/1/next"> > </a></th>
         <th class="spacer"></th>
-        <th><div class="yw-widget-text" data-yw-url="YOUR_URL_HERE"><script src="/widgets/yesterweb.js"></script></div></th>
+        <th><div class="yw-widget-text nowrap" data-yw-url="YOUR_URL_HERE"><script src="/widgets/yesterweb.js"></script></div></th>
         
 
       </table>
@@ -32,7 +32,7 @@ class Webrings extends HTMLElement {
   }
 }
 
-customElements.define('webrings-component', Webrings);
+customElements.define('webring-list-component', Webrings);
 
 var imported = document.createElement('script');
 imported.src = 'https://graystea.neocities.org/CozyRing/compact-text-widget.js';
@@ -41,25 +41,3 @@ document.head.appendChild(imported);
 var imported = document.createElement('script');
 imported.src = '/widgets/yesterweb.js';
 document.head.appendChild(imported);
-
-
-/*
-<main class="webrings-element">
-<div id='CozyRing'><script id="CozyRing" type="text/javascript" src="/CozyRing/compact-text-widget.js"></script></div>
-<ul>
-<li><a href="https://hotlinewebring.club/GraysTea/previous"> < </a></li>
-<li><a href="https://hotlinewebring.club/" target="_blank">Hotline</a></li>
-<li><a href="https://hotlinewebring.club/GraysTea/next"> > </a></li>
-</ul>
-<ul>
-<li><a href="https://webring.dinhe.net/prev/https://graystea.neocities.org/"> < </a></li>
-<li><a href="https://webring.dinhe.net" target="_blank"> Retronaut </a></li>
-<li><a href="https://webring.dinhe.net/next/https://graystea.neocities.org/"> > </a></li>
-</ul>
-<ul>
-<li><a href="http://geekring.net/site/NUMBER/previous"> < </a></li>
-<li><a href="https://geekring.net/" target="_blank"> GeekRing </a></li>
-<li><a href="http://geekring.net/site/NUMBER/next"> > </a></li>
-</ul>
-</main>
-*/
