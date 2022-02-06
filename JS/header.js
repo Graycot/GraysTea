26f4f9852e7dab44f1f8833d31114ff2fcb74acd
Gray <https://discord.com/invite/JM34yvMaFP>
2022-02-06 02:53:42 -0500

    added content, min-height 95vh to .main-element to prevent unrendered space

--- JS/header.js
+++ JS/header.js
@@ -9,14 +9,15 @@ class Header extends HTMLElement {
     <header class="header-element">
       <nav class="desktop-menu">
           <a class="WIP nav-button-1 nav-button" href="/index.html">Home</a>
-          <a class="WIP nav-button-2 nav-button" href="/index.html">Cool Online Places</a>
-          <a class="WIP nav-button-3 nav-button" href="/content/grays-tea/about.html" >Join Cozy Ring</a>
-          <a class="WIP nav-button-4 nav-button" href="/content/grays-tea/CozyRing/CozyRing.html">Educational Resources</a>
-          <a class="WIP nav-button-5 nav-button" href="#">Web Dev Guide</a>
-          <a class="WIP nav-button-6 nav-button" href="#" >Useful Tools</a>
-          <a class="WIP nav-button-7 nav-button" href="#">Personal Interests</a>
+          <a class="WIP nav-button-2 nav-button" href="/content/grays-tea/cool-online-places.html">Cool Online Places</a>
+          <a class="WIP nav-button-3 nav-button" href="/content/grays-tea/join-cozy-ring.html" >Join Cozy Ring</a>
+          <a class="WIP nav-button-4 nav-button" href="/content/grays-tea/educational-resources.html">Educational Resources</a>
+          <a class="WIP nav-button-5 nav-button" href="/content/grays-tea/web-dev-guide.html">Web Dev Guide</a>
+          <a class="WIP nav-button-6 nav-button" href="/content/grays-tea/useful-tools.html" >Useful Tools</a>
+          <a class="WIP nav-button-7 nav-button" href="/content/grays-tea/personal-interests.html">Personal Interests</a>
           <a class="WIP nav-button-8 nav-button" href="/content/grays-tea/about.html">About</a>
           <a class="WIP nav-button-9 nav-button" href="/content/grays-tea/contact.html">Contact</a>
+          <a class="WIP nav-button-10 nav-button" href="/content/grays-tea/entertainment.html">Entertainment</a>
       </nav>
     </header>
 
