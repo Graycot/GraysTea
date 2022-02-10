2eb784a7abc86bb2b8513ad921ed6bfb543c89a1
Gray <https://discord.com/invite/JM34yvMaFP>
2022-02-10 00:40:07 -0500

    webring navigation

--- JS/header.js
+++ JS/header.js
@@ -8,15 +8,16 @@ class Header extends HTMLElement {
 
     <header class="header-element">
       <nav class="desktop-menu">
-          <a class="WIP nav-button-1 nav-button" href="/index.html">Home</a>
-          <a class="WIP nav-button-2 nav-button" href="/content/grays-tea/cool-online-places.html">Cool Online Places</a>
-          <a class="WIP nav-button-3 nav-button" href="/content/grays-tea/join-cozy-ring.html" >Join Cozy Ring</a>
-          <a class="WIP nav-button-4 nav-button" href="/content/grays-tea/educational-resources.html">Educational Resources</a>
-          <a class="WIP nav-button-5 nav-button" href="/content/grays-tea/web-dev-guide.html">Web Dev Guide</a>
-          <a class="WIP nav-button-6 nav-button" href="/content/grays-tea/useful-tools.html" >Useful Tools</a>
-          <a class="WIP nav-button-7 nav-button" href="/content/grays-tea/entertainment.html">Entertainment</a>
-          <a class="WIP nav-button-8 nav-button" href="/content/grays-tea/about.html">About</a>
-          <a class="WIP nav-button-9 nav-button" href="/content/grays-tea/contact.html">Contact</a>
+          <a class="nav-button-1 nav-button" href="/index.html">Home</a>
+          <a class="nav-button-2 nav-button" href="/content/grays-tea/join-cozy-ring.html" >Join Cozy Ring</a>
+          <a class="nav-button-3 nav-button" href="/content/grays-tea/entertainment.html">Entertainment</a>
+          <a class="nav-button-4 nav-button" href="/content/grays-tea/cool-online-places.html">Cool Online Places</a>
+          <a class="nav-button-5 nav-button" href="/content/grays-tea/educational-resources.html">Educational Resources</a>
+          <a class="nav-button-6 nav-button" href="/content/grays-tea/useful-tools.html" >Useful Tools</a>
+          <a class="nav-button-7 nav-button" href="/content/grays-tea/my-projects.html">My Projects</a>
+          <a class="nav-button-8 nav-button" href="/content/grays-tea/about.html">About</a>
+          <a class="nav-button-9 nav-button" href="/content/grays-tea/contact.html">Contact</a>
+          <a class="nav-button-10 nav-button" href="/testing-zone.html">Testing Zone</a>
       </nav>
     </header>
 
