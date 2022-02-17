e21a189acd5b0d1b30932b39770eb26ca3f6a9b0
Gray <https://discord.com/invite/JM34yvMaFP>
2022-02-16 22:39:02 -0500

    change nav to webring-list, header to nav, sidebar to sidebar1, and chat to sidebar 2.

--- JS/nav.js
+++ JS/nav.js
@@ -1,4 +1,4 @@
-class Header extends HTMLElement {
+class nav extends HTMLElement {
   constructor() {
     super();
   }
@@ -6,7 +6,7 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
 
-    <header class="header-element">
+    <nav class="nav-element">
       <nav class="desktop-menu">
           <a class="nav-button-1 nav-button" href="/index.html">Home</a>
           <a class="nav-button-2 nav-button" href="/content/grays-tea/join-cozy-ring.html" >Join Cozy Ring</a>
@@ -14,16 +14,16 @@ class Header extends HTMLElement {
           <a class="nav-button-4 nav-button" href="/content/grays-tea/cool-online-places.html">Cool Online Places</a>
           <a class="nav-button-5 nav-button" href="/content/grays-tea/educational-resources.html">Educational Resources</a>
           <a class="nav-button-6 nav-button" href="/content/grays-tea/useful-tools.html" >Useful Tools</a>
-          <a class="nav-button-7 nav-button" href="/content/grays-tea/my-projects.html">My Projects</a>
+          <a class="WIP nav-button-7 nav-button" href="/content/grays-tea/my-projects.html">My Projects</a>
           <a class="nav-button-8 nav-button" href="/content/grays-tea/about.html">About</a>
           <a class="nav-button-9 nav-button" href="/content/grays-tea/contact.html">Contact</a>
-          <a class="WIP nav-button-10 nav-button" href="/template.html">Testing Zone</a>
+          <a class="nav-button-10 nav-button" href="/testing-zone.html">Testing Zone</a>
           <a class="WIP nav-button-11 nav-button" href="/content/grays-tea/clone-this-website.html">Clone this website</a>
       </nav>
-    </header>
+    </nav>
 
     `;
   }
 }
 
-customElements.define('header-component', Header);
+customElements.define('nav-component', nav);
